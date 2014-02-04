e906ba7fb86d3bcd86c9b58aa0a3b82e3455a757
Jürgen 'tante' Geuter <tante@tante.cc>
2014-02-05 00:51:16 +0100

    Dynamic length of urls list
    Also added comments and more pretty names to the extension.
    But the display doesn't look right yet.

--- youshouldknowbetter/contentscript.js
+++ youshouldknowbetter/contentscript.js
@@ -3,7 +3,7 @@ chrome.storage.sync.get("youshouldknowbetter",checkforblocks);
 function checkforblocks(response) {
     blockedurls = response['youshouldknowbetter'];
     for (var i=0; i<blockedurls.length; i++){
-        var pattern = RegExp(blockedurls[i],"i");
+        var pattern = RegExp(blockedurls[i].url,"i");
         if(pattern.test(window.location.href)){
             /* check if the referrer is from this domain. 
              * if not show the overlay */
@@ -21,13 +21,18 @@ function destroy_overlay(){
 }
 
 function show_overlay(url) {
-   var overlay = document.createElement("div");
-   overlay.setAttribute("class","overlay");
-   overlay.setAttribute("id","youshouldknowbetteroverlay");
-   overlay.innerHTML = chrome.i18n.getMessage("overlayHeading");
-   overlay.innerHTML += "<br/>";
-   overlay.innerHTML += chrome.i18n.getMessage("overlayQuestion",url);
-   overlay.innerHTML += "<br/>";
-   overlay.innerHTML += "<b><a href=\"javascript:document.getElementById('youshouldknowbetteroverlay').parentNode.removeChild(document.getElementById('youshouldknowbetteroverlay'));\">"+chrome.i18n.getMessage("overlayConfirmationButton")+"</a></b>";
-   document.body.appendChild(overlay);
+    var overlay = document.createElement("div");
+    overlay.setAttribute("class","overlay");
+    overlay.setAttribute("id","youshouldknowbetteroverlay");
+    overlay.innerHTML = '<div class="yskboverlayheader">'+chrome.i18n.getMessage("overlayHeading")+"</div>";
+    var displayname=url.url;
+    if(url.name){
+        displayname=url.name;
+    }
+    overlay.innerHTML += '<div class="yskboverlayquestion">'+chrome.i18n.getMessage("overlayQuestion",displayname)+"</div>";
+    if(url.comment){
+        overlay.innerHTML += '<div class="yskboverlaycommentHeader">'+chrome.i18n.getMessage("overlayCommentHeader")+':</div>'+'<div class="yskboverlaycommentText">'+url.comment+'</div><div class="yskboverlaycommentPostQuestion">'+chrome.i18n.getMessage("overlayCommentPostQuestion")+'</div>';
+    }
+    overlay.innerHTML += "<b><a href=\"javascript:document.getElementById('youshouldknowbetteroverlay').parentNode.removeChild(document.getElementById('youshouldknowbetteroverlay'));\">"+chrome.i18n.getMessage("overlayConfirmationButton")+"</a></b>";
+    document.body.appendChild(overlay);
 }
--- youshouldknowbetter/options.js
+++ youshouldknowbetter/options.js
@@ -1,33 +1,64 @@
 // load strings
-var strings =["optionsHeading","optionsPagesHeading","optionsURLexplanation","optionsButtonSave"];
+var strings =["optionsHeading","optionsPagesHeading","optionsURLexplanation","optionsButtonSave","tableHeadingURL","tableHeadingName","tableHeadingComment"];
 loadStrings(strings);
 
 // Saves options to localStorage.
 function save_options() {
-  var select = document.getElementById("urls");
-  var blockedurls = select.value.trim().split("\n");
-  chrome.storage.sync.set({"youshouldknowbetter":blockedurls});
+    var tbl = document.getElementById("urlsTable");
+    var blockedurls = [];
+    // we skip the heading row
+    for (var i=1, row; row=tbl.rows[i]; i++) {
+        var entry={"url":"","name":"","comment":""};
+        entry["url"]=row.cells[0].childNodes[0].value;
+        entry["name"]=row.cells[1].childNodes[0].value;
+        entry["comment"]=row.cells[2].childNodes[0].value;
+        blockedurls.push(entry);
+    }
 
-  // Update status to let user know options were saved.
-  var status = document.getElementById("status");
-  status.innerHTML = chrome.i18n.getMessage("optionsSaved");
-  setTimeout(function() {
-    status.innerHTML = "";
-  }, 750);
+    chrome.storage.sync.set({"youshouldknowbetter":blockedurls});
+
+    // Update status to let user know options were saved.
+    var status = document.getElementById("status");
+    status.innerHTML = chrome.i18n.getMessage("optionsSaved");
+    setTimeout(function() {
+        status.innerHTML = "";
+    }, 750);
 }
 
 // Restores select box state to saved value from localStorage.
 function restore_options() {
-  chrome.storage.sync.get("youshouldknowbetter",fill_textarea);
-
+    chrome.storage.sync.get("youshouldknowbetter",fill_table);
 }
 
+function fill_table(contents) {
+    var urls = contents['youshouldknowbetter'];
+    for (var i=0; i < urls.length; i++) {
+        // migration path for old url style
+        if (typeof(urls[i])=="string"){
+            urls[i]={"url":urls[i],"name":"","comment":""};
+        }
+        // create entry
+        var tbl = document.getElementById("urlsTable");
+        var newrow = tbl.insertRow(tbl.rows.length);
+        if (i%2==1){
+            newrow.className ="colored";
+        }
+        var newUrlCell = newrow.insertCell(0);
+        newUrlCell.innerHTML = '<input type="text" size="40" value="'+urls[i].url+'">';
+        var newNameCell = newrow.insertCell(1);
+        newNameCell.innerHTML = '<input type="text" size="40" value="'+urls[i].name+'">';
+        var newCommentCell = newrow.insertCell(2);
+        newCommentCell.innerHTML = '<input type="text" size="40" value="'+urls[i].comment+'">';
+    }
+}
+
+// appends all urls to something textareay
 function fill_textarea(urls){
-  var textarea = document.getElementById("urls");
-  blockedurls = urls['youshouldknowbetter']
-  for (var i=0; i < blockedurls.length; i++){
-      textarea.value += blockedurls[i]+"\n";
-  }
+    var textarea = document.getElementById("urls");
+    blockedurls = urls['youshouldknowbetter']
+        for (var i=0; i < blockedurls.length; i++){
+            textarea.value += blockedurls[i]+"\n";
+        }
 }
 
 document.addEventListener('DOMContentLoaded', restore_options);
