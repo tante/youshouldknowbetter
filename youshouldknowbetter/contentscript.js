cc4449e91703321d5ffc323e4040383a193326c5
Jürgen 'tante' Geuter <tante@tante.cc>
2014-03-18 23:39:35 +0100

    Can now detect Authors and warn
    Found Author brings a popup (but just with the first author which is
    hacky).
    
    I need to define spaces in the overlay to fill upon demand so I can
    differentiate between Author trigger and URL trigger (and possibly other
    triggers as well).

--- youshouldknowbetter/contentscript.js
+++ youshouldknowbetter/contentscript.js
@@ -2,7 +2,9 @@ migrateSettings();
 chrome.storage.sync.get("youshouldknowbetter",checkforblocks);
 
 function checkforblocks(response) {
-    console.log(response);
+    // we store the different issues in a var to pass to the overlay formatter
+    var issues = {};
+    var issuefound = false;
     // check for blocked urls
     blockedurls = response['youshouldknowbetter']["urls"];
     for (var i=0; i<blockedurls.length; i++){
@@ -18,9 +20,32 @@ function checkforblocks(response) {
              * if not show the overlay */
             var ref = document.referrer;
             if(!pattern.test(ref)){
-                show_overlay(blockedurls[i]);
+                issues["url"] = blockedurls[i];
+                issuefound = true;
             }
         }
+    }
+    // check for author
+    blockedauthors = response["youshouldknowbetter"]["authors"];
+    page_authors = find_authors();
+    tmpauthors = [];
+    var authorfound = false;
+    for (var i = 0, blocks = blockedauthors.length; i < blocks; i++) {
+        for (var j = 0, pauthors = page_authors.length; j < pauthors; j++) {
+            if(page_authors[j].search(blockedauthors[i]['name'])>-1){
+                author=blockedauthors[i];
+                author["foundas"]=page_authors[j];
+                tmpauthors.push(author);
+                authorfound = true;
+                issuefound = true;
+            }
+        }
+    }
+    if (authorfound) {
+        issues["authors"]=tmpauthors;
+    }
+    if (issuefound){
+        show_overlay(issues);
     }
 }
 
@@ -32,7 +57,7 @@ function checkforblocks(response) {
  *   * maybe a few more
  * returns the author names or an empty array if none found
  */
-function find_author(){
+function find_authors(){
     // look for schema.org metainfo
     authors = [];
     iterator = document.evaluate( '//*[@itemprop]', document, null, XPathResult.ANY_TYPE, null );
@@ -51,7 +76,6 @@ function find_author(){
         }
         node=iterator.iterateNext();
     }
-    
     // look for meta tag <meta name="author" ...>
     metatags = document.getElementsByTagName("meta");
     for(i=0;i<metatags.length;i++){
@@ -59,15 +83,8 @@ function find_author(){
             authors.push(metatags[i].getAttribute("content"));
         }
     }
-    
     return authors;
 }
-
-function log_author(){
-    console.log("Author: "+find_author())
-}
-
-window.onload = log_author();
 
 /* 
  * Remove the overlay 
@@ -81,24 +98,44 @@ function destroy_overlay(){
  * Show the overlay
  * #TODO Needs serious cleanup
  */
-function show_overlay(url) {
+function show_overlay(issues) {
+    console.log(issues);
     var overlay = document.createElement("div");
     overlay.setAttribute("class","yskboverlay");
     overlay.setAttribute("id","youshouldknowbetteroverlay");
     overlay.innerHTML = '<div class="yskboverlayheader">'+chrome.i18n.getMessage("overlayHeading")+"</div>";
-    // old format migration fix
-    if (url.url){
-        var displayname=url.url;
+    
+    var displayname = false;
+    var displaycomment = false;
+    // See if its about a blocked url
+    if (issues.url){
+        displayname=issues.url.url;
+        // if the url has a pretty name show that one
+        if(issues.url.name){
+            displayname=issues.url.name;
+        }
     }
-    else{
-        var displayname=url;
+    // if the url has an associated comment show that one
+    if(displayname && issues.url.comment){
+        displaycomment=issues.url.comment;
+    } 
+
+    // now check for author issues
+    if(displayname && issues.authors){
+        displayname += " "+chrome.i18n.getMessage("and")+" ";
+        displayname += issues.authors[0].name;
     }
-    if(url.name){
-        displayname=url.name;
+
+    if(!displayname && issues.authors){
+        displayname = issues.authors[0].name + " (found as:"+issues.authors[0].foundas+")";
+        if(issues.authors[0].comment){
+            displaycomment=issues.authors[0].comment;
+        }
     }
+    
     overlay.innerHTML += '<div class="yskboverlayquestion">'+chrome.i18n.getMessage("overlayQuestion",displayname)+"</div>";
-    if(url.comment){
-        overlay.innerHTML += '<div class="yskboverlaycommentHeader">'+chrome.i18n.getMessage("overlayCommentHeader")+':</div>'+'<div class="yskboverlaycommentText">'+url.comment+'</div><div class="yskboverlaycommentPostQuestion">'+chrome.i18n.getMessage("overlayCommentPostQuestion")+'</div>';
+    if(displaycomment){
+        overlay.innerHTML += '<div class="yskboverlaycommentHeader">'+chrome.i18n.getMessage("overlayCommentHeader")+':</div>'+'<div class="yskboverlaycommentText">'+displaycomment+'</div><div class="yskboverlaycommentPostQuestion">'+chrome.i18n.getMessage("overlayCommentPostQuestion")+'</div>';
     }
     overlay.innerHTML += "<button onclick=\"javascript:document.getElementById('youshouldknowbetteroverlay').parentNode.removeChild(document.getElementById('youshouldknowbetteroverlay'));\">"+chrome.i18n.getMessage("overlayConfirmationButton")+"</button>";
     document.body.appendChild(overlay);
